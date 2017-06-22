431ef17a9e15a4bda3834f51bc5a7a465ad7cc1c
Gazaunga <Gazaunga@gmail.com>
2017-06-22 15:35:38 -0400

    Create cw.sh

---
+++ bin/.scripts/cw.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+feh --randomize --bg-fill $HOME/Pictures/Wallpapers/*
+
+# Save this script and edit your with the command "crontab -e" (it launches an editor where you put this line at the end of the file):
+
+# */1     *     *     *     *         /bin/bash $HOME/bin/.scripts/cw.sh
